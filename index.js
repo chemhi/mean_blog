951f53756ea19b58a665c427baa5a700f17a38df
chemhi <chemhi@hotmail.com>
2017-01-02 14:28:59 +0900

    Installing and Setting the Mongoose DB connection to the project

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 var express = require('express');
+var mongoose = require('mongoose');
 var app = express();
 
+// DB Setting
+mongoose.connect(process.env.MONGO_DB);
+var db = mongoose.connection;
+
+db.once('open', function(){
+  console.log("DB connected");
+});
+
+db.on("error", function(err){
+  console.log("DB ERROR : ", err);
+});
+
+//other settings
 app.set("view engine", "ejs");  //1
 app.use(express.static(__dirname + '/public'));
 
