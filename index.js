af63c82e6a3be97ecd108bbffe122fb16369df5d
chemhi <chemhi@hotmail.com>
2017-01-02 13:44:11 +0900

    Making dynamic website with ejs + modifying css file

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static(__dirname + '/public')); //1
+app.set("view engine", "ejs");  //1
+app.use(express.static(__dirname + '/public'));
+
+app.get("/hello", function(req, res){
+  res.render("hello", {name:req.query.nameQuery});
+});
+
+app.get("/hello/:nameParam", function(req, res){
+  res.render("hello", {name:req.params.nameParam});
+});
 
 app.listen(3000, function(){
   console.log('Server On!');
