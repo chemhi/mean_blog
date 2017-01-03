30622c427762e051156e2f0fb9b3098bbc260486
chemhi <chemhi@hotmail.com>
2017-01-03 14:41:31 +0900

    Generate Address Book functioning with Index, New, Create

--- index.js
+++ index.js
@@ -1,29 +1,69 @@
-var express = require('express');
-var mongoose = require('mongoose');
-var app = express();
+// index.js
 
-// DB Setting
+var express = require('express');           // simple and convenient on express.js
+var mongoose = require('mongoose');         // mongo DB
+var bodyParser = require('body-parser');    // Using body-parser module
+var app = express();                        // Makeing use of the express object
+
+// DB Setting ...
+// bring the mongoose env value from the host PC and Open its DB
 mongoose.connect(process.env.MONGO_DB);
-var db = mongoose.connection;
+var db = mongoose.connection;               // The mongoose Connection constructor
+// => var db =mongoose.createConnection(process.env.MONGO_DB);
 
-db.once('open', function(){
-  console.log("DB connected");
+// DB openning
+db.once('open', function(){                 // open the mongo DB
+  console.log("DB connected");              // openning log
 });
 
+// emitting the error function for the DB
 db.on("error", function(err){
   console.log("DB ERROR : ", err);
 });
 
-//other settings
+//Specifying the ejs module as a view engine
 app.set("view engine", "ejs");  //1
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public')); // default directory
+app.use(bodyParser.json());                     // can be only useable with the json type
+app.use(bodyParser.urlencoded({extended:true}));  // encoding the url as the readable type
 
-app.get("/hello", function(req, res){
-  res.render("hello", {name:req.query.nameQuery});
+// DB schema
+var contactSchema = mongoose.Schema({
+  name:{type:String, required:true, unique:true},
+  email:{type:String, required:true, unique:true},
+  phone:{type:String}
+});
+// Generate the 'Contact' object model on the name of contact which has contact Schema
+// contact_model : Schema model name,
+// Contact : an Object instance to be used in the whole this code.
+var Contact = mongoose.model('contact_model', contactSchema);
+
+// Routes
+// Redirection to the default Route
+app.get('/', function(req, res){    // be always redirected to /contacts
+  res.redirect("/contacts");
 });
 
-app.get("/hello/:nameParam", function(req, res){
-  res.render("hello", {name:req.params.nameParam});
+// Show me the current indices
+// items : Chunk of the DB data
+// contacts_obj : a name of the argument to pass to the ejs forms.
+app.get('/contacts', function(req, res){
+  Contact.find({}, function(err, items){  // Contact.find will refer to the DB
+    if(err) return res.json(err);         // if error, show the error in json type
+    res.render('contacts/index', {contacts_obj:items});
+          // pass 'items' to the contacts/index.ejs on the name of 'contacts_obj'
+  });
+});
+// Make the new forms
+app.get('/contacts/new', function(req, res){
+  res.render("contacts/new");     //watching the contacts/new.ejs
+});
+// POST the new form to the DB
+app.post('/contacts', function(req, res){
+  Contact.create(req.body, function(err, contact){  //contact is ?
+    if(err) return res.json(err);
+    res.redirect('/contacts');      // show the updated indices
+  });
 });
 
 app.listen(3000, function(){
